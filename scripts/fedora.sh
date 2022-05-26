242e78ec9734c9f68df3ba095b5e28f3f5d952c2
vukilis <vuklekic22@gmail.com>
2022-05-27 01:30:18 +0200

    script

--- scripts/fedora.sh
+++ scripts/fedora.sh
@@ -20,7 +20,7 @@ repo_pkgs(){
 ########## Update ##########
 repo_pkgs
 #sudo dnf check-update
-sudo dnf update
+sudo dnf -y update
 
 ########## Install rpm fusion ##########
 sudo dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
